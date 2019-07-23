0eda20a6eba4b4df456a5b422d9daa18122f6fc7
james <jameskong1019@gmail.com>
2019-07-22 22:56:30 -0600

    initial commit

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(cors());
-app.use(express.static('view'));
+// app.use(bodyParser.json());
+// app.use(cors());
+app.use(express.static('views'));
 
 const port = process.env.Port || 5000;
 
