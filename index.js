1e9f8fe4aa33191721035b3e422cfa88390e4f8c
james <jameskong1019@gmail.com>
2019-07-23 20:37:16 -0600

    apply layout

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.listen(process.env.PORT || 4000, function(){
-    console.log('Your node js server is running');
-});
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => console.log(`server started on port ${port}`));
