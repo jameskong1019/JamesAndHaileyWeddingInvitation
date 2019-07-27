1339a2cc41580f82cd085cedf1614e27614b0577
james <jameskong1019@gmail.com>
2019-07-26 21:29:49 -0600

    minor update

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const favicon = require('serve-favicon');
 
 app.use(express.static(path.join(__dirname, 'views')));
+app.use(favicon(path.join(__dirname, 'views/content/images/icon', 'favicon.ico')))
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'views/index.html'));
