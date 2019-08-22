cff8d30e89770dc06ecec14653dddc63ae380594
james <jameskong1019@gmail.com>
2019-08-21 20:26:00 -0600

    update

--- views/js/main.js
+++ views/js/main.js
@@ -14,6 +14,7 @@ $(document).ready(function () {
     $('.music').on('click', function (e) {
         e.preventDefault();
         var music = $(myaudio);
+        $(".bubble").hide();
         if (myaudio.paused) {
             $('#m_icon').attr('src', 'content/images/icon/music_on.png');
             myaudio.play();
